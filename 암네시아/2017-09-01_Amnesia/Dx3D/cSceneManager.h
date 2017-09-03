f39ea92f7a41527cc45e8a0897c6458e95283cc9
LEE <leehn5@naver.com>
2017-09-03 23:36:21 +0900

    cSceneManager g_pSceneManager 정의

--- 암네시아/2017-09-01_Amnesia/Dx3D/cSceneManager.h
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cSceneManager.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#define g_pSceneManager cSceneManager::GetInstance
+
 class cSceneManager
 {
 private:
 	SINGLETON(cSceneManager)
 
 public:
-	cSceneManager();
-	~cSceneManager();
 };
