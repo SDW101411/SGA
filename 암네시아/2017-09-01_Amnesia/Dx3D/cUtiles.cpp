390937966665c0c70efdf8502d4294eec2513a6f
LEE <leehn5@naver.com>
2017-09-04 00:24:02 +0900

    cUtiles 클래스 생성

---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cUtiles.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "cUtiles.h"
+
+cUtiles::cUtiles()
+{
+}
+
+cUtiles::~cUtiles()
+{
+}
---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cUtiles.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class cUtiles
+{
+private:
+
+public:
+	cUtiles();
+	~cUtiles();
+};
+
