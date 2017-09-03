fd8eeb9b01e9e76c98163a1929dcf194e225687d
LEE <leehn5@naver.com>
2017-09-03 22:40:52 +0900

    cTabUI 클래스 생성

---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cTabUI.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cTabUI.h"
+
+
+cTabUI::cTabUI()
+{
+}
+
+
+cTabUI::~cTabUI()
+{
+}
---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cTabUI.h
@@ -0,0 +1,8 @@
+#pragma once
+class cTabUI
+{
+public:
+	cTabUI();
+	~cTabUI();
+};
+
