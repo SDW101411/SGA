f096c9ba21648c62fe017fb50fe16c99e69d708c
남궁영빈 <sssd00123@naver.com>
2017-09-04 00:35:15 +0900

    AStar 기본 함수 및 SetUp구현

--- 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.cpp
@@ -1,3 +1,24 @@
 #include "stdafx.h"
 #include "cAStar.h"
 #include "cGridNode.h"
+
+cAStar::cAStar()
+{
+}
+
+cAStar::~cAStar()
+{
+}
+
+void cAStar::SetUp(D3DXVECTOR3 leftTop, int row, int col)
+{
+}
+
+void cAStar::Render()
+{
+}
+
+list<D3DXVECTOR3> cAStar::FindPath(D3DXVECTOR3 start, D3DXVECTOR3 end)
+{
+	return list<D3DXVECTOR3>();
+}
--- 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.h
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.h
@@ -1,13 +1,22 @@
 #pragma once
 
+#define TILE_SIZE 1
+
 class cAStarNode;
 
 class cAStar
 {
 private:
-	vector<cAStarNode*> m_nodeList;
+	vector<cAStarNode*>	m_nodeList;
+	D3DXVECTOR3			m_leftTopPos;
+	int					m_rowMax;
+	int					m_colMax;
 
 public:
+	cAStar();
+	~cAStar();
+	void SetUp(D3DXVECTOR3 leftTop, int row, int col);
+	void Render();
 
-	list<D3DXVECTOR3> FindPath;
+	list<D3DXVECTOR3> FindPath(D3DXVECTOR3 start, D3DXVECTOR3 end);
 };
