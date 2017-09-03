7ee48678be8a798ccf662eee310c80a56d675d89
남궁영빈 <sssd00123@naver.com>
2017-09-04 01:08:37 +0900

    cAStar Render함수 CreateNode함수 수정

--- 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cAStar.cpp
@@ -19,10 +19,20 @@ void cAStar::SetUp(D3DXVECTOR3 leftTop, int row, int col)
 
 void cAStar::Render()
 {
+	for (int row = 0; row < m_rowMax; row++)
+	{
+		for (int col = 0; col < m_colMax; col++)
+		{
+			m_nodeList[row][col]->Render();
+		}
+	}
 }
 
 void cAStar::CreateNode(D3DXVECTOR3 pos, int row, int col)
 {
+	cGridNode* pNode = new cGridNode;
+	pNode->SetUp(pos);
+	m_nodeList[row][col] = pNode;
 }
 
 list<D3DXVECTOR3> cAStar::FindPath(D3DXVECTOR3 start, D3DXVECTOR3 end)
