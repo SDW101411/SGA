4f71b92584ff29b7f8000bb96feed0435768bc70
LEE <leehn5@naver.com>
2017-09-03 21:10:08 +0900

    다시 cUIManager 클래스 생성

---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cUIManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cUIManager.h"
+
+
+cUIManager::cUIManager()
+{
+}
+
+
+cUIManager::~cUIManager()
+{
+}
---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cUIManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class cUIManager
+{
+public:
+	cUIManager();
+	~cUIManager();
+};
+
