345892111e6f122f2e76b218a8aed72acc44cdb0
SDW-PC\SDW <apif730@gmail.com>
2017-09-04 01:53:15 +0900

    cMain_admin 헤더 추가

--- 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "cMain_admin.h"
+#include "cScene.h"
 /*2017-09-02 오후 1시 25분 메인 게임 입니다. 
 이곳에서 부터 게임제작을 시작 하도록 합시다. 
 cMain_Game에서 가져온 값들 Update와 Render, MsgProc 
@@ -18,6 +19,7 @@ cMain_admin::~cMain_admin()
 
 void cMain_admin::Update()
 {
+	
 }
 
 void cMain_admin::Render()
