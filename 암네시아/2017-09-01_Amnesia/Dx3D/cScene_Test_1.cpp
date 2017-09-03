3dc3896fb68bf5210f56a6dbf7e9668ef1789c5b
SDW-PC\SDW <apif730@gmail.com>
2017-09-04 02:20:25 +0900

    한나가 원하는거 추가 cScene 테스트 중

---
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cScene_Test_1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "cScene_Test_1.h"
+
+
+cScene_Test_1::cScene_Test_1()
+{
+}
+
+
+cScene_Test_1::~cScene_Test_1()
+{
+}
+
+void cScene_Test_1::Update()
+{
+}
+
+void cScene_Test_1::Render()
+{
+}
--- 암네시아/2017-09-01_Amnesia/Dx3D/cScene_Test_1.h
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cScene_Test_1.h
@@ -1,9 +1,16 @@
 #pragma once
 #include "cScene.h"
+
+/*2017-09-04 2시 16분
+cScene 상속 관계를 위한 테스트 중입니다.*/ 
+
 class cScene_Test_1:public cScene
 {
 public:
 	cScene_Test_1();
 	~cScene_Test_1();
+
+	void Update();
+	void Render();
 };
 
