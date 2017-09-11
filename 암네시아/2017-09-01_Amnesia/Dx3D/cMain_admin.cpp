feff05f73f58a371e9e2c70f41684c9609ea2af4
SDW <apif730@gmail.com>
2017-09-11 22:47:38 +0900

    내꺼 주석처리

--- 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
@@ -14,7 +14,7 @@
 cMain_admin::cMain_admin()
 	:m_pNow_Scene(NULL)
 {
-	m_pNow_Scene = new cScene_Test_1; // 1차 테스트 완료 상속에 대한 기본 테스트
+	//m_pNow_Scene = new cScene_Test_1; // 1차 테스트 완료 상속에 대한 기본 테스트
 	//m_pNow_Scene = new cMapToolScene; // 맵툴 테스트 입니다 - 영빈 -
 }
 
