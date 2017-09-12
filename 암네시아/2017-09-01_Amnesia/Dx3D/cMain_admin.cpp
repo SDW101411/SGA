0e0407f956e8c998e6eba083412b58bb3b247dac
LEE <leehn5@naver.com>
2017-09-12 09:46:51 +0900

    Debug 불필요한 오브젝트들 지움

--- 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
@@ -15,8 +15,8 @@ cMain_admin::cMain_admin()
 	:m_pNow_Scene(NULL)
 {
 	//m_pNow_Scene = new cScene_Test_1; // 1차 테스트 완료 상속에 대한 기본 테스트
-	//m_pNow_Scene = new cMapToolScene; // 맵툴 테스트 입니다 - 영빈 -
-	m_pNow_Scene = new cUIToolTestScene; // UI 테스트 - LEE
+	m_pNow_Scene = new cMapToolScene; // 맵툴 테스트 입니다 - 영빈 -
+	//m_pNow_Scene = new cUIToolTestScene; // UI 테스트 - LEE
 }
 
 
