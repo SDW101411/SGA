c6117de8248f6d771393d130749c58512b21d5c4
SDW <apif730@gmail.com>
2017-10-07 16:37:35 +0900

    cPlayer.cpp 의 손 애니메이션 속도를 수정하였습니다.

--- 암네시아/2017-09-01_Amnesia/Dx3D/cPlayer.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cPlayer.cpp
@@ -125,38 +125,6 @@ void cPlayer::Update()
 	if(m_PTarget_Mesh)Animation_Change();
 	cLight_Object_Picking_Update();
 	cObject_Item_OutLine_Update();
-
-	if (KEYMANAGER->isStayKeyDown('I'))
-	{
-		x += 0.01f;
-	}
-	if (KEYMANAGER->isStayKeyDown('O'))
-	{
-		y += 0.01f;
-	}
-	if (KEYMANAGER->isStayKeyDown('P'))
-	{
-		z += 0.01f;
-	}
-	if (KEYMANAGER->isStayKeyDown('K'))
-	{
-		k += 0.001f;
-	}
-	if (KEYMANAGER->isStayKeyDown('L'))
-	{
-		l += 0.001f;
-	}
-	if (KEYMANAGER->isStayKeyDown('H'))
-	{
-		k -= 0.001f;
-	}
-	if (KEYMANAGER->isStayKeyDown('J'))
-	{
-		l -= 0.001f;
-	}
-
-
-	int D = 0;
 }
 
 void cPlayer::Render()
