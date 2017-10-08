f7f6ca17ad94ac97da24d6802588c721664d6a51
SDW <apif730@gmail.com>
2017-10-09 03:31:48 +0900

    c_Gaussian 에 Wave 셰이더 추가하였습니다.
    n과 M으로 조절 가능합니다
    
    n을 누르면 일렁임이 증가합니다.
    
    m을 누르면 일렁임이 감소 합니다.

--- 암네시아/2017-09-01_Amnesia/Dx3D/c_Gaussian.h
+++ 암네시아/2017-09-01_Amnesia/Dx3D/c_Gaussian.h
@@ -2,10 +2,21 @@
 class c_Gaussian
 {
 protected:
+	
 	vector<ST_RHWT_VERTEX>	m_vecVertex;
+
 	LPD3DXEFFECT			m_pEffect;
 	LPDIRECT3DTEXTURE9		m_pTempRenderTarget;
 	LPDIRECT3DSURFACE9		m_pTempDepthStencil;
+
+
+
+	vector<ST_RHWT_VERTEX>	m_vecVertex_Wave;
+
+	LPD3DXEFFECT			m_pEffect_Wave;
+	LPDIRECT3DTEXTURE9		m_pTempRenderTarget_Wave;
+	LPDIRECT3DSURFACE9		m_pTempDepthStencil_Wave;
+
 public:
 	c_Gaussian();
 	~c_Gaussian();
@@ -17,6 +28,8 @@ public:
 	void c_Gaussian_setup();
 	void Render_Start(LPDIRECT3DSURFACE9 & pHWBackBuffer, LPDIRECT3DSURFACE9 & pHWDepthStencilBuffer, LPDIRECT3DSURFACE9 & pTempSurface);
 	
-	void Render_End(LPDIRECT3DSURFACE9 & pHWBackBuffer, LPDIRECT3DSURFACE9 & pHWDepthStencilBuffer, LPDIRECT3DSURFACE9 &pTempSurface);
+	void Render_End(LPDIRECT3DSURFACE9 & pHWBackBuffer, LPDIRECT3DSURFACE9 & pHWDepthStencilBuffer, LPDIRECT3DSURFACE9 &pTempSurface,
+		LPDIRECT3DSURFACE9 & pHWBackBuffer_2, LPDIRECT3DSURFACE9 & pHWDepthStencilBuffer_2, LPDIRECT3DSURFACE9 &pTempSurface_2
+	);
 };
 
