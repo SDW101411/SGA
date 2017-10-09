ab5e01a936194bf2ca0939dbd5f56514f7027f92
LEE <leehn5@naver.com>
2017-10-09 20:21:06 +0900

    cCursorStatus cScene 상속받았던거 지움, cMain_admin cCursorStatus 부분 지움

--- 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
+++ 암네시아/2017-09-01_Amnesia/Dx3D/cMain_admin.cpp
@@ -52,7 +52,6 @@ cMain_admin::cMain_admin()
 	g_pSceneManager->INSERT("cLoadingScene", new cLoadingScene);
 	g_pSceneManager->INSERT("cScene_Light", new cScene_Light);
 	g_pSceneManager->INSERT("cDamegeImpact", new cDamegeImpact);
-	g_pSceneManager->INSERT("cCursorStatus", new cCursorStatus);
 	g_pSceneManager->INSERT("cScene_Shader_Scene_Test", new cScene_Shader_Scene_Test);
 
 	g_pSceneManager->SceneChange("cMainMenuScene");
